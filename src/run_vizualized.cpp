9ab91faeaddfb01599054601bd85eae99985841c
Werner Kroneman <werner@wernerkroneman.nl>
2021-08-25 11:31:02 +0200

    Added CCD. Renamed executables.

--- src/run_vizualized.cpp
+++ src/run_vizualized.cpp
@@ -9,6 +9,7 @@
 #include <robowflex_library/io/broadcaster.h>
 #include <ompl/base/objectives/MechanicalWorkOptimizationObjective.h>
 #include <random_numbers/random_numbers.h>
+#include <moveit/collision_detection_bullet/collision_detector_allocator_bullet.h>
 #include "EndEffectorConstraintSampler.h"
 #include "build_request.h"
 #include "build_planning_scene.h"
@@ -16,6 +17,41 @@
 #include "make_robot.h"
 
 using namespace robowflex;
+
+class BulletContinuousMotionValidator : public ompl::base::MotionValidator {
+
+    std::shared_ptr<Scene> rb_scene_;
+    std::shared_ptr<Robot> rb_robot_;
+public:
+    BulletContinuousMotionValidator(ompl::base::SpaceInformation *si,
+                                    const std::shared_ptr<Robot> &rbRobot,
+                                    const std::shared_ptr<Scene> &rbScene)
+            : MotionValidator(si), rb_robot_(rbRobot), rb_scene_(rbScene) {}
+
+    bool checkMotion(const ompl::base::State *s1, const ompl::base::State *s2) const override {
+
+        collision_detection::CollisionResult res;
+        collision_detection::CollisionRequest req;
+
+        auto st1 = rb_robot_->allocState();
+        si_->getStateSpace()->as<ompl_interface::ModelBasedStateSpace>()->copyToRobotState(*st1, s1);
+
+        auto st2 = rb_robot_->allocState();
+        si_->getStateSpace()->as<ompl_interface::ModelBasedStateSpace>()->copyToRobotState(*st2, s2);
+
+        rb_scene_->getScene()->getCollisionEnv()->checkRobotCollision(req, res, *st1, *st2, rb_scene_->getACM());
+
+        return ! res.collision;
+    }
+
+    bool checkMotion(const ompl::base::State *s1, const ompl::base::State *s2,
+                     std::pair<ompl::base::State *, double> &lastValid) const override {
+        ROS_ERROR("Not implemented.");
+
+        return false;
+    }
+
+};
 
 int main(int argc, char **argv) {
 
@@ -34,6 +70,9 @@ int main(int argc, char **argv) {
 
     rviz.updateScene(scene);
 
+    scene->getScene()->setActiveCollisionDetector(collision_detection::CollisionDetectorAllocatorBullet::create(),
+                                                  true);
+
     auto simple_planner = std::make_shared<OMPL::OMPLInterfacePlanner>(drone, "simple");
 
     OMPL::Settings settings;
@@ -49,12 +88,18 @@ int main(int argc, char **argv) {
             .registerSamplerAllocator(std::make_shared<DroneStateConstraintSamplerAllocator>());
 
     simple_planner->setPreplanCallback([&](){
+
         const ompl::geometric::SimpleSetupPtr &ss = simple_planner->getLastSimpleSetup();
         ss->setOptimizationObjective(
                 std::make_shared<ClearanceDecreaseMinimzationObjective>(ss->getSpaceInformation())
                 );
+
+        ss->getSpaceInformation()->setMotionValidator(std::make_shared<BulletContinuousMotionValidator>(ss->getSpaceInformation().get(), drone, scene));
+
         std::cout << "Objective set." << std::endl;
     });
+
+
 
     ompl::msg::setLogLevel(ompl::msg::LOG_INFO);
 
