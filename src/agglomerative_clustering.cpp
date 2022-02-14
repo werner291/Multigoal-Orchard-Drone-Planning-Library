a820f6be3ec16aeb0ff4f8a77d489d712353f06d
Werner Kroneman <werner@wernerkroneman.nl>
2022-02-14 10:10:05 +0100

    Worked on the enhanced HAC algo.

--- src/agglomerative_clustering.cpp
+++ src/agglomerative_clustering.cpp
@@ -1,5 +1,150 @@
-//
-// Created by werner on 10-02-22.
-//
 
 #include "agglomerative_clustering.h"
+#include <boost/range/adaptor/transformed.hpp>
+
+agglomerative_clustering::AgglomerativeClustering::AgglomerativeClustering(
+        const std::vector<ompl::base::ScopedStatePtr> &toCluster, const PointToPointPlanner &ptp)
+        : next_node_id(0), ptp(ptp) {
+    _nodes = boost::copy_range<std::vector<TreeNode>>(
+            toCluster | boost::adaptors::transformed([&](const ompl::base::ScopedStatePtr &state) {
+                return TreeNode{next_node_id++, state, {}};
+            })
+    );
+}
+
+bool agglomerative_clustering::AgglomerativeClustering::iterate() {
+
+    assert(_nodes.size() >= 2);
+
+    merge_pair(CandidatePair());
+
+    return _nodes.size() == 1;
+
+}
+
+void agglomerative_clustering::AgglomerativeClustering::merge_pair(CandidatePair pair) {
+    assert(pair.is_tight());
+
+    _nodes.erase(pair.pair.first);
+    _nodes.erase(pair.pair.second);
+
+    _nodes.insert({
+                         next_node_id++,
+                         {
+                                 pair.midpoint,
+                                 {pair.pair}
+                         }
+                 });
+
+    // How to update the candidate pairs? TODO
+    assert(false);
+}
+
+agglomerative_clustering::AgglomerativeClustering::CandidatePair
+agglomerative_clustering::AgglomerativeClustering::extract_next_valid_candidate() {
+
+    CandidatePair candidate;
+
+    do {
+        auto candidate = _candidate_pairs.top();
+        _candidate_pairs.pop();
+    } while (isNotClosed(candidate));
+
+    return candidate;
+}
+
+bool agglomerative_clustering::AgglomerativeClustering::isNotClosed(
+        const agglomerative_clustering::AgglomerativeClustering::CandidatePair &candidate) const {
+    return _nodes.count(candidate.pair.first) && _nodes.count(candidate.pair.second);
+}
+
+agglomerative_clustering::AgglomerativeClustering::CandidatePair
+agglomerative_clustering::AgglomerativeClustering::find_closest_pair() {
+    auto candidate = extract_next_valid_candidate();
+
+    while (!candidate.is_tight()) {
+
+        tighten_and_enqueue(candidate.pair.first, candidate.pair.second);
+
+        candidate = extract_next_valid_candidate();
+    }
+
+    return candidate;
+}
+
+void agglomerative_clustering::AgglomerativeClustering::tighten_and_enqueue(size_t a, size_t b) {
+    if (auto path = ptp.planToOmplState(0.2, _nodes[a].representative, _nodes[b].representative)) {
+        auto midpoint = std::make_shared<ompl::base::ScopedState<ompl::base::StateSpace>>(
+                ptp.getPlanner()->getSpaceInformation());
+        ptp->getPlanner()->getSpaceInformation()->copyState(midpoint->get(), path->getState(path->getNumStates() / 2));
+        _candidate_pairs.emplace({a, b}, ptp->length(), midpoint);
+    } else {
+        ROS_WARN("Cannot plan between candidate pair.");
+    }
+}
+
+void agglomerative_clustering::AgglomerativeClustering::pick_pivots(size_t n) {
+    std::vector<size_t> choices;
+    choices.reserve(n);
+
+    ompl::RNG rng;
+
+    for (size_t i: boost::irange(0, n)) {
+
+        size_t choice = rng.uniformInt(0, _nodes.size() - choices.size());
+
+        for (size_t j: choices) {
+            if (choice >= j) {
+                choice += 1;
+            }
+        }
+
+        choices.push_back(choice);
+    }
+
+    _pivots = boost::copy_range<>(choices | boost::adaptors::transformed([&](size_t node_id) {
+        assert(_nodes.count(node_id) == 1);
+        return _nodes[node_id].representative;
+    }));
+}
+
+void agglomerative_clustering::AgglomerativeClustering::computeInitialEstimates(size_t k_pivots) {
+
+    pick_pivots(k_pivots);
+
+
+
+    for (const auto&[node_i, _]: _nodes) {
+        for (const auto&[node_j, _]: _nodes) {
+            if (node_j >= node_i) continue;
+            double distance_bound = INFINITY;
+            for (size_t pivot_i: boost:irange(0, k_pivots)) {
+                distance_bound = std::min(distance_bound,
+                                          pivot_distances[pivot_i][node_i] + pivot_distances[pivot_j][node_i]);
+            }
+            _candidate_pairs.push({
+                                         {node_i, node_j},
+                                         distance_bound,
+                                         {}
+                                 });
+        }
+    }
+
+
+}
+
+void agglomerative_clustering::AgglomerativeClustering::compute_pivot_distance_matrix() {
+    for (size_t pivot_i: boost:irange(0, k_pivots)) {
+        auto pivot = _pivots[pivot_i];
+
+        for (const auto&[node_id, node]: _nodes) {
+            if (auto path = ptp.planToOmplState(0.2, pivot->get(), node.represenattive->get())) {
+                pivot_distances[pivot_i][node_id] = path->length();
+            } else {
+                pivot_distances[pivot_i][node_id] = INFINITY;
+            }
+        }
+    }
+}
+
+
--- src/agglomerative_clustering.h
+++ src/agglomerative_clustering.h
@@ -11,61 +11,124 @@
 
 namespace agglomerative_clustering {
 
-    // Worth considering: https://www.researchgate.net/publication/220895309_Speeding-Up_Hierarchical_Agglomerative_Clustering_in_Presence_of_Expensive_Metrics
+    /**
+     * Enhanced agglomerative clustering algorithm based on:
+     * https://www.researchgate.net/publication/220895309_Speeding-Up_Hierarchical_Agglomerative_Clustering_in_Presence_of_Expensive_Metrics
+     */
+    class AgglomerativeClustering {
 
-    struct TreeNode {
-        size_t dedup;
-        ompl::base::ScopedStatePtr representative;
-        std::optional<std::pair<std::shared_ptr<TreeNode>,std::shared_ptr<TreeNode>>> children;
-    };
+        // Keeps track of the next node ID that can be assigned uniquely.
+        size_t next_node_id;
+        // How to plan from state-to-state.
+        const PointToPointPlanner& ptp;
 
-    TreeNode enhancedHierarchicalAgglomerativeClustering(const std::vector<ompl::base::ScopedStatePtr>& to_cluster,
-                                                         PointToPointPlanner ptp) {
+    public:
+        /**
+         * A node in the clustering tree. Leaf nodes have empty children.
+         */
+        struct TreeNode {
+            // A state that represents the rough center of the cluster.
+            // All cluster members are supposed to be reachable from it.
+            ompl::base::ScopedStatePtr representative;
 
-        struct QueueEntry {
-            double lower_bound;
-            bool is_exact;
-            ompl::base::ScopedStatePtr potential_representative;
-            std::pair<std::shared_ptr<TreeNode>,std::shared_ptr<TreeNode>> pair;
+            // Child nodes, which are empty for leaf nodes.
+            std::optional<std::pair<std::shared_ptr<TreeNode>,std::shared_ptr<TreeNode>>> children;
         };
 
-        std::priority_queue<QueueEntry> queue;
+        /**
+         * A pair of node IDs that are potential children of a new node in the tree.
+         */
+        struct CandidatePair {
+            // IDs of the nodes to be combined.
+            const std::pair<size_t,size_t> pair;
+            // An upper bound on the path-planning distance between the node's representatives.
+            const double upper_distance_bound;
+            // If available, a state from with both node representatives are reachable,
+            // forming a rough center point of the hypothetical cluster to be formed.
+            // May be null, in which case the `upper_distance_bound` is not a tight bound.
+            const ompl::base::ScopedStatePtr midpoint;
 
-        std::unordered_set<size_t> eliminated;
-
-        while (!queue.empty()) {
-
-            QueueEntry top = queue.top();
-            queue.pop();
-
-            // Lazy-delete.
-            if (eliminated.count(top.pair.first->dedup) == 1 || eliminated.count(top.pair.second->dedup) == 1) {
-                continue;
+            /**
+             * Whether `upper_distance_bound` is a tight bound obtained by an actual planner,
+             * or merely an upper bound based on pivot distances.
+             */
+            bool is_tight() {
+                // midpoint is available if and only if this pair was planned for.
+                return midpoint->get() != nullptr;
             }
 
-            if (top.is_exact) {
-
-
-
-            } else {
-                auto result = ptp.planToOmplState(0.2,top.pair.first->representative->get(),top.pair.second->representative->get());
-
-                if (result) {
-
-                    top.lower_bound = result->length();
-                    top.is_exact = true;
-                    top.potential_representative = std::make_shared<ompl::base::ScopedState<ompl::base::StateSpace>>(ptp.getPlanner()->getSpaceInformation());
-                    ptp.getPlanner()->getSpaceInformation()->copyState(top.potential_representative->get(), result->getState(result->getStateCount()/2));
-
-                } else {
-                    ROS_WARN("Dropped a state pair.");
-                }
-
+            /**
+             * Ordering, such that the priority queue puts lowest-distance pairs first.
+             */
+            bool operator<(const CandidatePair& other) {
+                return upper_distance_bound < other.upper_distance_bound;
             }
+        };
 
-        }
+    private:
+        /// Keeps track of all nodes that can be combined.
+        /// Algorithm is done once this is a singleton.
+        std::unordered_map<size_t,TreeNode> _nodes;
 
-    }
+        /// Queue of candidate nodes to combine, ordered by distance upper bound.
+        /// Note: uses lazy deletion: check candidates returned for whether that node is still open!
+        std::priority_queue<CandidatePair> _candidate_pairs;
+
+        /// A set of reference states that enable us to skip a lot of expensive, exact distance computations.
+        std::vector<ompl::base::ScopedStatePtr> _pivots;
+
+        std::vector<std::unordered_map<size_t, double>> pivot_distances(k_pivots);
+
+    public:
+        AgglomerativeClustering(
+                const std::vector<ompl::base::ScopedStatePtr> &toCluster,
+                const PointToPointPlanner &ptp);
+
+        /**
+         * Pulls the head of the queue, repeating and discarding pairs
+         * whose nodes have been closed according to `closed_set`.
+         *
+         * Modifies the queue to remove the pairs returned or discarded.
+         */
+        CandidatePair extract_next_valid_candidate();
+
+        /**
+         * Runs the point-to-point planner between the representatives of the nodes,
+         * and pushes a CandidatePair with exact-known distance onto the queue.
+         */
+        void tighten_and_enqueue(size_t a, size_t b);
+
+        /**
+         * Extracts the CandidatePair with the lowest distance, whose distance must be exact.
+         *
+         * Exact distances are computed as necessary.
+         */
+        CandidatePair find_closest_pair();
+
+        /**
+         * Pick the IDs of which nodes to use as pivots, usually at the start of the algorithm.
+         */
+        void pick_pivots(size_t n);
+
+        /**
+         *
+         */
+        void compute_pivot_distance_matrix();
+
+        /**
+         * Populates the queue of candidate_pairs by computing the k*n distance matrix,
+         * where `n` is the number of open pairs and `k` the number of pivots.
+         *
+         * @param k_pivots Number of pivots.
+         */
+        void computeInitialEstimates(size_t k_pivots);
+
+        void merge_pair(CandidatePair pair);
+
+        bool iterate();
+
+        bool isNotClosed(const CandidatePair &candidate) const;
+    };
 
 }
 
