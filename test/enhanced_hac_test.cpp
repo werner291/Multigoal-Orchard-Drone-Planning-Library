5c3196f790b5959dee3d82b9b8ab5e5e1a17295e
Werner Kroneman <w.kroneman@ucr.nl>
2022-02-15 14:52:45 +0100

    Implemented a version of EHAC. Performance is... euh... Not great.

--- test/enhanced_hac_test.cpp
+++ test/enhanced_hac_test.cpp
@@ -1,5 +1,22 @@
-
+#include <gtest/gtest.h>
+#include <boost/range/irange.hpp>
+#include <ompl/util/RandomNumbers.h>
+#include <json/value.h>
+#include <fstream>
 #include "../src/agglomerative_clustering.h"
+#include "../src/json_utils.h"
+
+Json::Value toJSON(const agglomerative_clustering::AgglomerativeClustering<double>::TreeNode& result) {
+    Json::Value val;
+
+    val["representative"] = result.representative;
+    if (result.children) {
+        val["children"][0] = toJSON(*(result.children->first));
+        val["children"][1] = toJSON(*(result.children->second));
+    }
+
+    return val;
+}
 
 TEST(EnhancedHACTest, hac_test) {
 
@@ -13,8 +30,26 @@ TEST(EnhancedHACTest, hac_test) {
 
     }
 
-    AgglomerativeClustering<double> aggl(
-            []()
-            );
+    using namespace agglomerative_clustering;
+
+    size_t expensive_calls = 0;
+
+    // 1079150
+    // 4000000
+
+    AgglomerativeClustering<double>::ExpensiveDistanceFn distance = [&](double a, double b) -> AgglomerativeClustering<double>::DistanceResult {
+            expensive_calls += 1;
+            return {abs(a-b),(a+b)/2.0};
+    };
+
+    AgglomerativeClustering<double> clustering(distance, points, 20);
+
+    AgglomerativeClustering<double>::TreeNode result = clustering.run();
+
+    std::cout << "Expensive calls: " << expensive_calls << " (n^2 = " << std::pow(points.size(),2) << ")" << std::endl;
+
+    std::ofstream ofs("analysis/ehac.json");
+    ofs << toJSON(result);
+    ofs.close();
 
 }
