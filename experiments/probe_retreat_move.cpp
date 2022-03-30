b5bb6308799f73e070cc90995a6b625985a71eda
Werner Kroneman <w.kroneman@ucr.nl>
2022-03-30 15:16:08 +0200

    Minor refactoring.

--- experiments/probe_retreat_move.cpp
+++ experiments/probe_retreat_move.cpp
@@ -8,66 +8,10 @@
 #include "../src/traveling_salesman.h"
 #include "../src/general_utilities.h"
 #include "../src/msgs_utilities.h"
+#include "../src/ros_utilities.h"
 
 #include <range/v3/all.hpp>
 #include <boost/format.hpp>
-#include <eigen_conversions/eigen_msg.h>
-
-void dumpApproaches(const moveit::core::RobotModelPtr &drone, const std::shared_ptr<DroneStateSpace> &state_space,
-                    const std::shared_ptr<ompl::base::SpaceInformation> &si,
-                    const std::vector<std::pair<Apple, ompl::geometric::PathGeometric>> &approaches,
-                    ros::NodeHandle &nh, const std::string &topic_name);
-
-ros::Publisher dumpProjections(const std::vector<std::pair<Apple, ompl::geometric::PathGeometric>> &apples,
-                               const SphereShell &sphereShell,
-                               ros::NodeHandle &nh,
-                               const std::string &topic_name,
-                               std_msgs::ColorRGBA color) {
-    auto sphere_projections = nh.advertise<visualization_msgs::MarkerArray>(topic_name, 1, true);
-
-    visualization_msgs::MarkerArray projections_msg;
-
-    visualization_msgs::Marker balls;
-    balls.header.frame_id = "world";
-    balls.type = visualization_msgs::Marker::POINTS;
-    balls.points.resize(apples.size());
-    balls.color = color;
-    balls.scale.x = 0.05;
-    balls.scale.y = 0.05;
-    balls.scale.z = 0.05;
-    balls.pose.orientation.w = 1.0;
-
-    visualization_msgs::Marker lines;
-    lines.header.frame_id = "world";
-    lines.id = 1;
-    lines.type = visualization_msgs::Marker::LINE_LIST;
-    lines.scale.x = 0.01;
-    lines.color = color;
-    lines.pose.orientation.w = 1.0;
-    lines.points.resize(2 * apples.size());
-
-    for (const auto &[apple_id, apple]: apples | ranges::views::enumerate) {
-        moveit::core::RobotState rs(apple.second.getSpaceInformation()->getStateSpace()->as<DroneStateSpace>()->getRobotModel());
-
-        apple.second.getSpaceInformation()->getStateSpace()->as<DroneStateSpace>()->copyToRobotState(rs, apple.second.getState(0));
-
-        rs.update(true);
-
-        const Eigen::Vector3d onSphere = rs.getGlobalLinkTransform("end_effector").translation();
-        tf::pointEigenToMsg(onSphere, balls.points[apple_id]);
-
-        tf::pointEigenToMsg(apple.first.center, lines.points[apple_id * 2]);
-        tf::pointEigenToMsg(onSphere, lines.points[apple_id * 2 + 1]);
-
-    }
-
-    projections_msg.markers.push_back(balls);
-    projections_msg.markers.push_back(lines);
-
-    sphere_projections.publish(projections_msg);
-
-    return sphere_projections;
-}
 
 int main(int argc, char **argv) {
 
@@ -123,19 +67,25 @@ int main(int argc, char **argv) {
     };
 
     for (const auto &[approach_type, approaches, color] : orderings) {
-        dumpApproaches(drone, state_space, si, approaches, nh, "/approaches_"+approach_type);
+        publisher_handles.push_back(dumpApproaches(drone, state_space, si, approaches, nh, "/approaches_"+approach_type));
 
         auto apples_after_approach = approaches | ranges::views::transform([](auto pair) {return pair.first;}) | ranges::to_vector;
 
         auto approaches_by_gcd = vectorByOrdering(approaches , ORToolsOrderingStrategy().apple_ordering(apples_after_approach, gdh));
 
-        publisher_handles.push_back(dumpProjections(approaches, sphereShell, nh, "/apple_to_sphere_projections_"+approach_type, color));
+        publisher_handles.push_back(dumpProjections(approaches, nh, "/apple_to_sphere_projections_" + approach_type,
+                                                    color));
 
         auto fullPath = planFullPath(si, start.get(), shell, approaches_by_gcd);
+
+        std::cout << "Full path (" << approach_type << ") length: " << fullPath.length() << std::endl;
+
         auto moveit_trajectory = omplPathToRobotTrajectory(drone, state_space, fullPath);
+
         moveit_msgs::DisplayTrajectory msg = robotTrajectoryToDisplayTrajectory(moveit_trajectory);
         auto traj = nh.advertise<moveit_msgs::DisplayTrajectory>("/trajectory_"+approach_type, 1, true);
         traj.publish(msg);
+        publisher_handles.push_back(traj);
     }
 
     std::cout << "ready" << std::endl;
@@ -145,23 +95,4 @@ int main(int argc, char **argv) {
 }
 
 
-
-void dumpApproaches(const moveit::core::RobotModelPtr &drone, const std::shared_ptr<DroneStateSpace> &state_space,
-                    const std::shared_ptr<ompl::base::SpaceInformation> &si,
-                    const std::vector<std::pair<Apple, ompl::geometric::PathGeometric>> &approaches,
-                    ros::NodeHandle &nh, const std::string &topic_name) {
-
-    ompl::geometric::PathGeometric combined_path(si);
-    for (const auto &approach: approaches) {
-        combined_path.append(approach.second);
-    }
-
-    moveit_msgs::DisplayTrajectory msg = robotTrajectoryToDisplayTrajectory(omplPathToRobotTrajectory(
-            drone, state_space, combined_path
-    ));
-
-    auto traj = nh.advertise<moveit_msgs::DisplayTrajectory>(topic_name, 1, true);
-
-    traj.publish(msg);
-}
 
