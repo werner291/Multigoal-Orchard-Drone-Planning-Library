ea16d2520015623aa1e51d0d29965562bb1643d8
Werner Kroneman <w.kroneman@ucr.nl>
2022-05-30 08:58:49 +0200

    Ran a full test run for TSP-over-PRM

--- experiments/prm_multigoal_experiment.cpp
+++ experiments/prm_multigoal_experiment.cpp
@@ -13,21 +13,34 @@ using namespace ranges;
 
 int main(int argc, char **argv) {
 
-    const auto samples_per_goal = ranges::views::iota(2,10);
-    const double plan_times_seconds[] = {1.0, 2.0, 5.0, 10.0};//, 15.0};//, 20.0};//, 30.0, 60.0};
+    const auto samples_per_goal = ranges::views::iota(2, 10);
+    const double plan_times_seconds[] = {1.0, 2.0, 5.0, 10.0, 15.0, 20.0, 30.0, 60.0};
     const bool optimize_segments_options[] = {true};//{false,true};
 
     auto allocators = ranges::views::cartesian_product(plan_times_seconds, samples_per_goal, optimize_segments_options)
-        | ranges::views::transform([&](const auto tuple) -> NewMultiGoalPlannerAllocatorFn {
+                      | ranges::views::filter([](const auto tuple) {
+        const auto &[plan_time, samples, optimize_segments] = tuple;
+
+        double expected_time = plan_time * (double) (samples * samples);
+
+        if (expected_time >= 600.0) {
+            std::cout << "Dropping task with plan_time: " << plan_time << " samples: " << samples << " expected_time: "
+                      << expected_time << std::endl;
+            return false;
+        } else {
+            return true;
+        }
+
+    }) | ranges::views::transform([&](const auto tuple) -> NewMultiGoalPlannerAllocatorFn {
         return [tuple = tuple](const AppleTreePlanningScene &scene_info,
                                const ompl::base::SpaceInformationPtr &si) {
-            return std::make_shared<MultigoalPrmStar>(get<0>(tuple),get<1>(tuple),get<2>(tuple));
+            return std::make_shared<MultigoalPrmStar>(get<0>(tuple), get<1>(tuple), get<2>(tuple));
         };
     }) | to_vector;
 
     ompl::msg::setLogLevel(ompl::msg::LOG_ERROR);
 
-    run_planner_experiment(allocators, "analysis/prm_multigoal_experiment.json", 10);
+    run_planner_experiment(allocators, "analysis/prm_multigoal_experiment.json", 50);
 
     return 0;
 }
