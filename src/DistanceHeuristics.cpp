716c35794d0ad30dd6daa84f4b120383fa98f175
Werner Kroneman <w.kroneman@ucr.nl>
2022-07-19 13:33:07 +0200

    Docs and reformatting.

--- src/DistanceHeuristics.cpp
+++ src/DistanceHeuristics.cpp
@@ -1,47 +1,68 @@
-
 #include "DistanceHeuristics.h"
 
 #include <utility>
 
 double EuclideanOmplDistanceHeuristics::state_to_goal(const ompl::base::State *a, const ompl::base::Goal *b) const {
-    moveit::core::RobotState sta(state_space_->getRobotModel());
 
-    state_space_->copyToRobotState(sta, a);
+	// Convert to a MoveIt state.
+	moveit::core::RobotState sta(state_space_->getRobotModel());
+	state_space_->copyToRobotState(sta, a);
 
-    Eigen::Vector3d end_effector_pos = sta.getGlobalLinkTransform("end_effector").translation();
+	// Apply forward kinematics to get the end-effector position.
+	Eigen::Vector3d end_effector_pos = sta.getGlobalLinkTransform("end_effector").translation();
 
-    return (end_effector_pos - b->as<DroneEndEffectorNearTarget>()->getTarget()).norm();
+	// Take the Euclidean distance.
+	return (end_effector_pos - b->as<DroneEndEffectorNearTarget>()->getTarget()).norm();
 }
 
 double EuclideanOmplDistanceHeuristics::goal_to_goal(const ompl::base::Goal *a, const ompl::base::Goal *b) const {
-    return (a->as<DroneEndEffectorNearTarget>()->getTarget() - b->as<DroneEndEffectorNearTarget>()->getTarget()).norm();
+
+	// Upcast the two goals to DroneEndEffectorNearTarget, get the target positions
+	Eigen::Vector3d a_tgt = a->as<DroneEndEffectorNearTarget>()->getTarget();
+	Eigen::Vector3d b_tgt = b->as<DroneEndEffectorNearTarget>()->getTarget();
+
+	// Then take the Euclidean distance.
+	return (a_tgt - b_tgt).norm();
 }
 
 EuclideanOmplDistanceHeuristics::EuclideanOmplDistanceHeuristics(std::shared_ptr<DroneStateSpace> stateSpace)
-        : state_space_(std::move(stateSpace)) {}
+		: state_space_(std::move(stateSpace)) {
+}
 
 std::string EuclideanOmplDistanceHeuristics::name() const {
-    return "Euclidean";
+	return "Euclidean";
 }
 
 double GreatCircleOmplDistanceHeuristics::state_to_goal(const ompl::base::State *a, const ompl::base::Goal *b) const {
-    moveit::core::RobotState sta(state_space_->getRobotModel());
 
-    state_space_->copyToRobotState(sta, a);
+	// Convert to a MoveIt state.
+	moveit::core::RobotState sta(state_space_->getRobotModel());
+	state_space_->copyToRobotState(sta, a);
 
-    Eigen::Vector3d end_effector_pos = sta.getGlobalLinkTransform("end_effector").translation();
+	// Apply forward kinematics to get the end-effector position.
+	Eigen::Vector3d end_effector_pos = sta.getGlobalLinkTransform("end_effector").translation();
 
-    return gcm.measure(end_effector_pos, b->as<DroneEndEffectorNearTarget>()->getTarget());
+	// Extract the target position.
+	Eigen::Vector3d b_tgt = b->as<DroneEndEffectorNearTarget>()->getTarget();
+
+	// Hand off to GreatCircleMetric
+	return gcm.measure(end_effector_pos, b_tgt);
 }
 
 double GreatCircleOmplDistanceHeuristics::goal_to_goal(const ompl::base::Goal *a, const ompl::base::Goal *b) const {
-    return gcm.measure(a->as<DroneEndEffectorNearTarget>()->getTarget(), b->as<DroneEndEffectorNearTarget>()->getTarget());
+
+	// Extract the target positions.
+	Eigen::Vector3d a_tgt = a->as<DroneEndEffectorNearTarget>()->getTarget();
+	Eigen::Vector3d b_tgt = b->as<DroneEndEffectorNearTarget>()->getTarget();
+
+	return gcm.measure(a_tgt, b_tgt);
 }
 
 GreatCircleOmplDistanceHeuristics::GreatCircleOmplDistanceHeuristics(GreatCircleMetric gcm,
-                                                                     std::shared_ptr<DroneStateSpace> stateSpace)
-        : gcm(std::move(gcm)), state_space_(std::move(stateSpace)) {}
+																	 std::shared_ptr<DroneStateSpace> stateSpace) : gcm(
+		std::move(gcm)), state_space_(std::move(stateSpace)) {
+}
 
 std::string GreatCircleOmplDistanceHeuristics::name() const {
-    return "GreatCircle";
+	return "GreatCircle";
 }
--- src/DistanceHeuristics.h
+++ src/DistanceHeuristics.h
@@ -1,4 +1,3 @@
-
 #ifndef NEW_PLANNERS_DISTANCEHEURISTICS_H
 #define NEW_PLANNERS_DISTANCEHEURISTICS_H
 
@@ -7,44 +6,118 @@
 #include "ompl_custom.h"
 #include "greatcircle.h"
 
+/**
+ * Base class for some notion of distance heuristics between either a state and a goal, or two goals.
+ */
 class OmplDistanceHeuristics {
 
 public:
-    virtual double state_to_goal(const ompl::base::State *, const ompl::base::Goal *) const = 0;
+	/**
+	 * Compute a heuristic distance between a given state and a goal.
+	 */
+	virtual double state_to_goal(const ompl::base::State *, const ompl::base::Goal *) const = 0;
 
-    virtual double goal_to_goal(const ompl::base::Goal *, const ompl::base::Goal *) const = 0;
+	/**
+	 * Compute a heuristic distance between two goals.
+	 */
+	virtual double goal_to_goal(const ompl::base::Goal *, const ompl::base::Goal *) const = 0;
 
-    [[nodiscard]] virtual std::string name() const = 0;
+	/**
+	 * Get the name of this heuristic.
+	 */
+	[[nodiscard]] virtual std::string name() const = 0;
 };
 
+/**
+ * Heuristic that uses the Euclidean distance. Assumes that the robot is a MoveIt robot model,
+ * and has a link called "end-effector", and that goals are DroneEndEffectorNearTarget.
+ */
 class EuclideanOmplDistanceHeuristics : public OmplDistanceHeuristics {
 
-    std::shared_ptr<DroneStateSpace> state_space_;
+	/// Keep a reference to the DroneStateSpace so that we can easily convert.
+	std::shared_ptr<DroneStateSpace> state_space_;
 
 public:
-    EuclideanOmplDistanceHeuristics(std::shared_ptr<DroneStateSpace> stateSpace);
+	/**
+	 * Constructor.
+	 * @param stateSpace Reference to the state space to convert between OMPL and MoveIt.
+	 */
+	EuclideanOmplDistanceHeuristics(std::shared_ptr<DroneStateSpace> stateSpace);
 
-    double state_to_goal(const ompl::base::State *a, const ompl::base::Goal *b) const override;
+	/**
+	 * Compute a heuristic distance between a given state and a goal. In this case, that is the Euclidean distance
+	 * between the end-effector of the robot and the goal.
+	 *
+	 * @param a The state of the robot.
+	 * @param b The goal, assumed to be a DroneEndEffectorNearTarget.
+	 *
+	 * @return The computed distance.
+	 */
+	double state_to_goal(const ompl::base::State *a, const ompl::base::Goal *b) const override;
 
-    double goal_to_goal(const ompl::base::Goal *a, const ompl::base::Goal *b) const override;
+	/**
+	 * Compute a heuristic distance between two goals. In this case, that is the Euclidean distance
+	 * between the two goals, assumed to be DroneEndEffectorNearTarget.
+	 *
+	 * @param a 	The first goal.
+	 * @param b 	The second goal.
+	 * @return 		The computed distance.
+	 */
+	double goal_to_goal(const ompl::base::Goal *a, const ompl::base::Goal *b) const override;
 
-    [[nodiscard]] std::string name() const override;
+	/**
+	 * @return The name of the heuristic ( "Euclidean" )
+	 */
+	[[nodiscard]] std::string name() const override;
 
 };
 
+/**
+ * Heuristic that uses the Great Circle distance. Assumes that the robot is a MoveIt robot model,
+ * and has a link called "end-effector", and that goals are DroneEndEffectorNearTarget.
+ */
 class GreatCircleOmplDistanceHeuristics : public OmplDistanceHeuristics {
 
-    GreatCircleMetric gcm;
-    std::shared_ptr<DroneStateSpace> state_space_;
+	/// Underlying GreatCircleMetric to compute the great-circle distance between two points in R^3.
+	GreatCircleMetric gcm;
+
+	/// Keep a reference to the DroneStateSpace so that we can easily convert.
+	std::shared_ptr<DroneStateSpace> state_space_;
 
 public:
-    GreatCircleOmplDistanceHeuristics(GreatCircleMetric gcm, std::shared_ptr<DroneStateSpace> stateSpace);
 
-    double state_to_goal(const ompl::base::State *a, const ompl::base::Goal *b) const override;
+	/**
+	 * Constructor.
+	 * @param gcm 			The GreatCircleMetric to use.
+	 * @param stateSpace 	Reference to the state space to convert between OMPL and MoveIt.
+	 */
+	GreatCircleOmplDistanceHeuristics(GreatCircleMetric gcm, std::shared_ptr<DroneStateSpace> stateSpace);
 
-    double goal_to_goal(const ompl::base::Goal *a, const ompl::base::Goal *b) const override;
+	/**
+	 * Compute a heuristic distance between a given state and a goal. In this case, that is the Great Circle distance
+	 * between the end-effector of the robot and the goal.
+	 *
+	 * @param a 	The state of the robot.
+	 * @param b  	The goal, assumed to be a DroneEndEffectorNearTarget.
+	 * @return 		The computed distance.
+	 */
+	double state_to_goal(const ompl::base::State *a, const ompl::base::Goal *b) const override;
 
-    [[nodiscard]] std::string name() const override;
+	/**
+	 * Compute the heuristic distance between two goals. In this case, that is the Great Circle distance
+	 * between the two goals, assumed to be DroneEndEffectorNearTarget.
+	 *
+	 * @param a 	The first goal.
+	 * @param b 	The second goal.
+	 *
+	 * @return 		The computed distance.
+	 */
+	double goal_to_goal(const ompl::base::Goal *a, const ompl::base::Goal *b) const override;
+
+	/**
+	 * @return The name of the heuristic ( "GreatCircle" )
+	 */
+	[[nodiscard]] std::string name() const override;
 
 };
 
