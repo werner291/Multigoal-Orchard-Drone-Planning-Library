d61bf7eee52e1048a3b3e23d19ff3a771d72fac6
Werner Kroneman <w.kroneman@ucr.nl>
2022-11-29 15:58:37 +0100

    Added documentation

--- src/experiments/ScannedSurfaceTracker.cpp
+++ src/experiments/ScannedSurfaceTracker.cpp
@@ -5,18 +5,21 @@
 #include "ScannedSurfaceTracker.h"
 
 ScannedSurfaceTracker ScannedSurfaceTracker::buildForMeshes(const std::vector<shape_msgs::msg::Mesh> &meshes) {
+	// Sample the points and call the constructor.
+	// (not doing this in the constructor because it would cause a RAII violation without relying on mutations)
 	return ScannedSurfaceTracker(buildScanTargetPoints(meshes, POINTS_PER_TARGET));
-
 }
 
 ScannedSurfaceTracker::ScannedSurfaceTracker(const std::vector<ScanTargetPoint> &points)
 		: points(points), scanned_points(points.size(), false), scannablePointsIndex(points) {
 
+	// Find the maximum target ID, which is expected to be at most in the several hundreds.
 	for (const auto &point: points) {
 		if (point.apple_id > max_target) {
 			max_target = point.apple_id;
 		}
 	}
+
 }
 
 void ScannedSurfaceTracker::snapshot(const Eigen::Isometry3d &sensor_pose,
@@ -29,9 +32,7 @@ void ScannedSurfaceTracker::snapshot(const Eigen::Isometry3d &sensor_pose,
 																	 SCAN_MAX_DISTANCE,
 																	 visible_points);
 
-
 	for (auto i: new_scanned_points) {
-
 		if (!scanned_points[i]) {
 			scanned_points[i] = true;
 		}
@@ -40,12 +41,17 @@ void ScannedSurfaceTracker::snapshot(const Eigen::Isometry3d &sensor_pose,
 }
 
 std::vector<double> ScannedSurfaceTracker::per_target_scan_portion() const {
+
+	// Initialize to 0 for all target meshes.
 	std::vector<double> per_fruit_scan_proportion(max_target + 1, 0.0);
 
+	// Iterate over all scan points.
 	for (size_t scan_point_id = 0; scan_point_id < scanned_points.size(); scan_point_id++) {
+		// If the point has been scanned, add to the proportion of the target mesh that has been scanned.
 		per_fruit_scan_proportion[points[scan_point_id].apple_id] +=
 				scanned_points[scan_point_id] / (double) POINTS_PER_TARGET;
 	}
 
+	// Return the proportion of each mesh that has been scanned.
 	return per_fruit_scan_proportion;
 }
--- src/experiments/ScannedSurfaceTracker.h
+++ src/experiments/ScannedSurfaceTracker.h
@@ -14,28 +14,68 @@
 /**
  * Given successive sensor poses and sets of scanned points, estimate
  * the percentage of the surface of each mesh that has been scanned.
+ *
+ * The class works by, for every mesh, sampling a number of points on the surface
+ * of the mesh.
+ *
+ * Then, by repeated calls to snapshot(), the class can be updated with the points
+ * that were scanned by the robot's camera. The class will keep track of which
+ * of the sampled points have been scanned.
  */
 class ScannedSurfaceTracker {
 
+	/// The maximum distance that a point can be from the robot's camera to be considered well-enough scanned.
 	const double SCAN_MAX_DISTANCE = 1.0;
+	/// The number of points to place on each mesh.
 	const size_t POINTS_PER_TARGET = 10;
+
+	/// The biggest target ID; using this, we can make the various vectors the right size.
 	size_t max_target = 0;
 
-	std::vector<bool> scanned_points;
 
+	/// The sampled points.
 	std::vector<ScanTargetPoint> points;
 
-	// Build a spatial index that can be used to quickly find what points are visible to the robot's camera.
+	/// For every sampled point, keep a boolean indicating whether it has been scanned.
+	std::vector<bool> scanned_points;
+
+	/// A spatial index that can be used to quickly find what points are visible to the robot's camera.
 	ScannablePointsIndex scannablePointsIndex;
 
+	/**
+	 * Constructor, with pre-sampled points. Refer to the buildForMeshes() method for a way to
+	 * generate these points and automatically wrap them in a ScannedSurfaceTracker.
+	 *
+	 * @param points 	The sampled points.
+	 */
 	explicit ScannedSurfaceTracker(const std::vector<ScanTargetPoint> &points);
 
 public:
+
+	/**
+	 * Build a ScannedSurfaceTracker for a set of meshes.
+	 *
+	 * @param meshes 		The meshes to build the ScannedSurfaceTracker for.
+	 * @return 				A ScannedSurfaceTracker for the given meshes.
+	 */
 	ScannedSurfaceTracker buildForMeshes(const std::vector<shape_msgs::msg::Mesh> &meshes);
 
+	/**
+	 * Update the tracker with a new snapshot of the robot's camera.
+	 *
+	 * @param sensor_pose 			The pose of the robot's camera.
+	 * @param visible_points 		The target points that the robot's camera can see.
+	 */
 	void
 	snapshot(const Eigen::Isometry3d &sensor_pose, const std::vector<SegmentedPointCloud::TargetPoint> &visible_points);
 
+	/**
+	 * Get the (approximate) proportion (0 to 1) of the surface of each mesh that has been scanned.
+	 *
+	 * The vector index corresponds to the vector of meshes that was passed to buildForMeshes().
+	 *
+	 * @return The vector of proportions, where the index is the mesh ID.
+	 */
 	std::vector<double> per_target_scan_portion() const;
 
 };
