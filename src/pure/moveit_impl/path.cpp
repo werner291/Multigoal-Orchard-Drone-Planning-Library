a54d2ebef5dad43258151fc93b270130ed03ee34
Werner Kroneman <w.kroneman@ucr.nl>
2023-06-13 11:52:03 +0200

    Created a, if I do say so myself, very clean re-implementation of the ShellSpacePlanner with templated code without any necessary external requirement on MoveIt, OMPL, or the ROS ecosystem. (It is of course implementable for those.)

---
+++ src/pure/moveit_impl/path.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2022 University College Roosevelt
+//
+// All rights reserved.
+
+//
+// Created by werner on 13-6-23.
+//
+
+#include "path.h"
---
+++ src/pure/moveit_impl/path.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 University College Roosevelt
+//
+// All rights reserved.
+
+//
+// Created by werner on 13-6-23.
+//
+
+#ifndef MGODPL_PATH_H
+#define MGODPL_PATH_H
+
+
+#endif //MGODPL_PATH_H
