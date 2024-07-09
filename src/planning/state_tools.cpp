0b67aab4c82ef17699af87ae7d5a2d88fb8212ee
Werner Kroneman <werner@wernerkroneman.nl>
2024-07-09 16:00:42 +0200

    Changed formatting

--- src/planning/state_tools.cpp
+++ src/planning/state_tools.cpp
@@ -12,26 +12,29 @@
 #include "spherical_geometry.h"
 
 mgodpl::RobotState mgodpl::fromEndEffectorAndVector(const mgodpl::robot_model::RobotModel &robot,
-													const mgodpl::math::Vec3d &endEffectorPoint,
-													const mgodpl::math::Vec3d &vector) {
-
+                                                    const mgodpl::math::Vec3d &endEffectorPoint,
+                                                    const mgodpl::math::Vec3d &vector) {
 	// Generate a state.
-	std::vector<double> arm_angles { -spherical_geometry::latitude(vector) };
+	std::vector<double> arm_angles{-spherical_geometry::latitude(vector)};
 
-	math::Transformd flying_base_tf {
-			.translation = math::Vec3d(0.0, 0.0, 0.0),
-			.orientation = math::Quaterniond::fromAxisAngle(math::Vec3d::UnitZ(), spherical_geometry::longitude(vector) + M_PI/2.0)
+	math::Transformd flying_base_tf{
+		.translation = math::Vec3d(0.0, 0.0, 0.0),
+		.orientation = math::Quaterniond::fromAxisAngle(math::Vec3d::UnitZ(),
+		                                                spherical_geometry::longitude(vector) + M_PI / 2.0)
 	};
 
-	const auto& fk = mgodpl::robot_model::forwardKinematics(robot, arm_angles, robot.findLinkByName("flying_base"), flying_base_tf);
+	const auto &fk = mgodpl::robot_model::forwardKinematics(robot,
+	                                                        arm_angles,
+	                                                        robot.findLinkByName("flying_base"),
+	                                                        flying_base_tf);
 
 	math::Vec3d end_effector_position = fk.forLink(robot.findLinkByName("end_effector")).translation;
 
 	flying_base_tf.translation = flying_base_tf.translation + endEffectorPoint - end_effector_position;
 
 	return {
-			.base_tf = flying_base_tf,
-			.joint_values = arm_angles,
+		.base_tf = flying_base_tf,
+		.joint_values = arm_angles,
 	};
 }
 
