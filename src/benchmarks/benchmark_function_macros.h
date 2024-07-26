ab436352ed0fe57d1d37794cdb80c02a02dbc05f
Werner Kroneman <werner@wernerkroneman.nl>
2024-07-26 16:04:46 +0200

    Added basic benchmark framework code.

---
+++ src/benchmarks/benchmark_function_macros.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2022 University College Roosevelt
+//
+// All rights reserved.
+
+//
+// Created by werner on 7-2-24.
+//
+
+#ifndef VISUALIZATION_FUNCTION_MACROS_H
+#define VISUALIZATION_FUNCTION_MACROS_H
+
+#include <functional>
+#include <map>
+#include <string>
+#include <filesystem>
+
+// A top-level function that can be called to visualize something.
+using BenchmarkFn = std::function<void()>;
+
+// A static map that maps a name to a visualization function.
+extern std::map<std::string, BenchmarkFn> benchmarks;
+
+#define REGISTER_BENCHMARK(name) \
+    void name(); \
+    static bool is_##name##_registered = [](){ \
+    benchmarks[#name] = name; \
+    return true; \
+    }(); \
+    void name()
+
+#endif //VISUALIZATION_FUNCTION_MACROS_H
---
+++ src/benchmarks/goal_sample_success_rate.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2024 University College Roosevelt
+//
+// All rights reserved.
+
+//
+// Created by werner on 7/26/24.
+//
+
+#include <iostream>
+
+#include "benchmark_function_macros.h"
+
+REGISTER_BENCHMARK(goal_sample_success_rate) {
+	std::cout << "Running goal_sample_success_rate" << std::endl;
+}
