b6ad8769b627c2f802e1184c4bb05629c0258107
Werner Kroneman <w.kroneman@ucr.nl>
2024-08-21 09:44:18 +0200

    Moved pick_indices_without_replacement to RandomNumberGenerator
    Signed-off-by: Werner Kroneman <w.kroneman@ucr.nl>

--- src/planning/RandomNumberGenerator.cpp
+++ src/planning/RandomNumberGenerator.cpp
@@ -6,7 +6,20 @@
 // Created by werner on 3/18/24.
 //
 
+#include <algorithm>
 #include "RandomNumberGenerator.h"
 
 namespace random_numbers {
+	std::vector<size_t> RandomNumberGenerator::pick_indices_without_replacement(size_t n, size_t k) {
+		assert(n >= k);
+
+		std::vector<size_t> indices(n);
+		std::iota(indices.begin(), indices.end(), 0);
+
+		std::shuffle(indices.begin(), indices.end(), generator);
+
+		indices.resize(k);
+
+		return indices;
+	}
 } // random_numbers
--- src/planning/RandomNumberGenerator.h
+++ src/planning/RandomNumberGenerator.h
@@ -65,6 +65,16 @@ namespace random_numbers {
 			std::uniform_real_distribution<double> distribution(min, max);
 			return distribution(generator);
 		}
+
+		/**
+		 * Pick k indices without replacement from the range 0..n. (Exclusive of n.)
+		 *
+		 * @param n		The number of indices to pick from.
+		 * @param k		The number of indices to pick.
+		 * @param rng	The random number generator to use.
+		 * @return		A vector of k indices.
+		 */
+		std::vector<size_t> pick_indices_without_replacement(size_t n, size_t k);
 	};
 } // random_numbers
 
