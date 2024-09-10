68a2fac5136e4808a822bdde19320eca90fcc316
Werner Kroneman <w.kroneman@ucr.nl>
2024-09-10 11:26:38 +0200

    Added a first attempt at scan path optimization.
    Signed-off-by: Werner Kroneman <w.kroneman@ucr.nl>

--- src/visualizations/scanning_motions.cpp
+++ src/visualizations/scanning_motions.cpp
@@ -32,6 +32,8 @@
 #include "../planning/cgal_chull_shortest_paths.h"
 #include "../visualization/CameraTracker.h"
 #include "../experiment_utils/point_scanning_evaluation.h"
+#include "../planning/local_optimization.h"
+#include "../math/AABB.h"
 
 using namespace mgodpl;
 using namespace mgodpl::vizualisation;
@@ -682,24 +684,155 @@ REGISTER_VISUALIZATION(scanning_motions_obstacle_avoidance) {
 	viewer.start();
 }
 
-//RobotPath optimize_scanpath(
-//    const RobotPath& path,
-//    const std::vector<ScannablePoints>& scan_points,
-//    const fcl::CollisionObjectd& tree_collision,
-//    const robot_model::RobotModel& robot_model,
-//    random_numbers::RandomNumberGenerator& rng)
-//{
-//
-//
-//	// Step 1: for every waypoint in the path, check a pool that says whether it scans a point or not:
-//	std::vector<bool> scans_point(path.size(), false);
-//
-//	for (size_t i = 0; i < path.size(); i++) {
-//
-//	}
-//
-//    return path;
-//}
+/**
+ * \struct OptimizeScanpathHooks
+ * \brief A structure to hold various hooks for the optimize_scanpath function.
+ *
+ * This structure contains several std::function members that can be used as hooks
+ * to monitor and interact with the scan path optimization process.
+ */
+struct OptimizeScanpathHooks {
+	/**
+	 * \brief Hook called when AABBs for clusters are computed.
+	 * \param aabbs A vector of computed AABBs.
+	 */
+	std::function<void(const std::vector<math::AABBd> &)> computed_aabbs;
+
+	/**
+	 * \brief Hook called at the beginning of mapping states to scan points.
+	 */
+	std::function<void()> begin_mapping_states_to_scan_points;
+
+	/**
+	 * \brief Hook called at the beginning of mapping a specific state to scan points.
+	 * \param state The current robot state being mapped.
+	 */
+	std::function<void(const RobotState &)> begin_mapping_state_to_scan_points;
+
+	/**
+	 * \brief Hook called at the beginning of mapping a state to a specific scan point cluster.
+	 * \param cluster_index The index of the scan point cluster.
+	 */
+	std::function<void(size_t)> begin_mapping_state_to_scan_point_cluster;
+
+	/**
+	 * \brief Hook called when a state scans a specific point.
+	 * \param state_index The index of the state.
+	 * \param point_index The index of the scan point.
+	 */
+	std::function<void(size_t, size_t)> state_scans_point;
+
+	/**
+	 * \brief Hook called when a state is outside the AABB.
+	 * \param state_index The index of the state.
+	 */
+	std::function<void(size_t)> state_outside_aabb;
+
+	/**
+	 * \brief Hook called at the end of mapping a state to a specific scan point cluster.
+	 * \param cluster_index The index of the scan point cluster.
+	 */
+	std::function<void(size_t)> end_mapping_state_to_scan_point_cluster;
+
+	/**
+	 * \brief Hook called at the end of mapping states to scan points.
+	 */
+	std::function<void()> end_mapping_states_to_scan_points;
+
+	/**
+	 * \brief Hook called when a waypoint will be deleted.
+	 * \param waypoint_index The index of the waypoint.
+	 * \param prev The previous state.
+	 * \param current The current state.
+	 * \param next The next state.
+	 */
+	std::function<void(size_t, const RobotState &, const RobotState &, const RobotState &)> will_delete_waypoint;
+};
+
+RobotPath optimize_scanpath(
+		RobotPath path,
+		const std::vector<ScannablePoints> &scan_points,
+		const fcl::CollisionObjectd &tree_collision,
+		const robot_model::RobotModel &robot_model,
+		random_numbers::RandomNumberGenerator &rng,
+		const std::optional<OptimizeScanpathHooks> &hooks = {}) {
+
+	// The path is trivial and cannot be optimized:
+	if (path.n_waypoints() < 3) {
+		return path;
+	}
+
+	const auto &aabbs = computeAABBsForClusters(scan_points);
+
+	if (hooks) hooks->computed_aabbs(aabbs);
+
+	if (hooks) hooks->begin_mapping_states_to_scan_points();
+
+	// Step 1: for every waypoint in the path, check a pool that says whether it scans a point or not:
+	std::vector<bool> scans_point(path.n_waypoints(), false);
+
+	// Step two: for every waypoint in the path, check if it is associated with any point in the scan points:
+	for (size_t i = 0; i < path.n_waypoints(); i++) {
+
+		const auto &state = path.waypoint(i);
+		const auto &fk = forwardKinematics(robot_model, state);
+		const auto &ee_pos = fk.forLink(robot_model.findLinkByName("end_effector")).translation;
+
+		if (hooks) hooks->begin_mapping_state_to_scan_points(state);
+
+		for (size_t j = 0; j < scan_points.size(); j++) {
+			const auto &scannable_points = scan_points[j];
+			const auto &aabb = aabbs[j];
+
+			if (hooks) hooks->begin_mapping_state_to_scan_point_cluster(j);
+
+			if (aabb.contains(ee_pos)) {
+				for (size_t point_index = 0; point_index < scannable_points.surface_points.size(); point_index++) {
+					if (is_visible(scannable_points, point_index, ee_pos)) {
+						scans_point[i] = true;
+						if (hooks) hooks->state_scans_point(j, point_index);
+						break;
+					}
+				}
+			} else {
+				if (hooks) hooks->state_outside_aabb(j);
+			}
+
+			if (hooks) hooks->end_mapping_state_to_scan_point_cluster(j);
+		}
+	}
+
+	if (hooks) hooks->end_mapping_states_to_scan_points();
+
+	const std::function motion_collides = [&](const RobotState &a, const RobotState &b) {
+		return check_motion_collides(robot_model, tree_collision, a, b);
+	};
+
+	// Step 3: for every waypoint that doesn't scan a point, try to delete it:
+	for (size_t wp_i = 1; wp_i + 1 < path.n_waypoints(); wp_i++) {
+		if (!scans_point[wp_i]) {
+
+			// Get the three states affected:
+			const auto &prev = path.states[wp_i - 1];
+			const auto &current = path.states[wp_i];
+			const auto &next = path.states[wp_i + 1];
+
+			if (!motion_collides(prev, next)) {
+
+				if (hooks) hooks->will_delete_waypoint(wp_i, prev, current, next);
+
+				// If the motion is collision-free, remove the waypoint:
+				path.states.erase(path.states.begin() + static_cast<long>(wp_i));
+				// Delete the entry in scans_point:
+				scans_point.erase(scans_point.begin() + static_cast<long>(wp_i));
+
+				wp_i -= 1; // Since we just deleted the waypoint, we need to recheck the current index.
+			}
+		}
+	}
+
+	return path;
+}
 
 REGISTER_VISUALIZATION(scanning_motions_for_each_fruit) {
 
@@ -782,14 +915,15 @@ REGISTER_VISUALIZATION(scanning_motions_for_each_fruit) {
 
 	const bool POST_OPTIMIZE = true;
 
-//	if (POST_OPTIMIZE) {
-//		path = optimize_scanpath(
-//				path,
-//				scan_points,
-//				tree_collision,
-//				robot_model,
-//				rng);
-//	}
+	if (POST_OPTIMIZE) {
+		double length_before = pathLength(path, equal_weights_distance);
+		path = optimize_scanpath(
+				path,
+				scan_points,
+				tree_collision,
+				robot_model,
+				rng);
+	}
 
 	// Record end time for measuring time taken:
 	auto end_time = std::chrono::high_resolution_clock::now();
