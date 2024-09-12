fdfe217f48af01490e95b4e6b97e0d8beee8d2be
Werner Kroneman <w.kroneman@ucr.nl>
2024-09-12 16:56:16 +0200

    Added makeshift_exponential_sampling visualization

--- src/visualizations/approach_planning.cpp
+++ src/visualizations/approach_planning.cpp
@@ -67,3 +67,69 @@ REGISTER_VISUALIZATION(nearby_sampling) {
 
     viewer.start();
 }
+
+
+REGISTER_VISUALIZATION(makeshift_exponential_sampling) {
+
+	// In this visualization, we will show the process of sampling nearby states and states along a motion.
+	const auto &robot = experiments::createProceduralRobotModel({
+																		.total_arm_length = 1.0,
+																		.joint_types = {experiments::JointType::HORIZONTAL},
+																});
+
+	RobotState start_state {
+			.base_tf = math::Transformd::identity(),
+			.joint_values = {0.0}
+	};
+
+	RobotState end_state {
+			.base_tf = {
+					.translation = {1, 1, 1},
+					.orientation = math::Quaterniond::fromAxisAngle(math::Vec3d::UnitZ(), 1.0)
+					},
+			.joint_values = {M_PI_2}
+	};
+
+	auto initial_fk = robot_model::forwardKinematics(robot, start_state);
+
+	for (int i = 0; i <= 10; ++i) {
+		double t = i / 10.0;
+		// Interpolate between the start and end state:
+		RobotState interpolated_state = interpolate(start_state, end_state, t);
+		auto fk = robot_model::forwardKinematics(robot, interpolated_state);
+		mgodpl::vizualisation::vizualize_robot_state(viewer, robot, fk, {1, 0, 1});
+	}
+
+	random_numbers::RandomNumberGenerator rng(42);
+
+	const double FACTOR = 2.0;
+
+	int samples = 0;
+
+	std::vector<vizualisation::RobotActors> last_actors;
+	const size_t KEEP = 10;
+
+	viewer.addTimerCallback([&](){
+
+		RobotState nearby_state = makeshift_exponential_sample_along_motion(start_state, end_state, rng, FACTOR);
+
+		auto fk = robot_model::forwardKinematics(robot, nearby_state);
+
+		while (last_actors.size() >= KEEP) {
+			for (auto &actor : last_actors.front().actors) {
+				viewer.removeActor(actor);
+			}
+			last_actors.erase(last_actors.begin());
+		}
+
+		// Draw the nearby state:
+		last_actors.push_back(mgodpl::vizualisation::vizualize_robot_state(viewer, robot, fk, {0, 1, 0}));
+
+	});
+
+	viewer.lockCameraUp();
+
+	viewer.setCameraTransform({5,5,1},{0,0,0});
+
+	viewer.start();
+}
