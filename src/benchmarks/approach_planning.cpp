1b9bb1dfd0539a202aa9af66d57537a995ee0020
Werner Kroneman <w.kroneman@ucr.nl>
2024-09-18 14:47:30 +0200

    Expanded approach_planning_comparison benchmark
    Signed-off-by: Werner Kroneman <w.kroneman@ucr.nl>

--- src/benchmarks/approach_planning.cpp
+++ src/benchmarks/approach_planning.cpp
@@ -10,10 +10,16 @@
 #include "benchmark_function_macros.h"
 #include "../experiment_utils/tree_benchmark_data.h"
 #include "../planning/RobotPath.h"
+#include "../experiment_utils/procedural_robot_models.h"
+#include "../planning/RandomNumberGenerator.h"
+
+import approach_by_pullout;
+#include <CGAL/Side_of_triangle_mesh.h>
 
 using namespace mgodpl;
 
 struct ApproachPlanningProblem {
+	const robot_model::RobotModel &robot;
 	const experiments::TreeModelBenchmarkData &tree_model;
 };
 
@@ -21,36 +27,71 @@ struct ApproachPlanningResults {
 	std::vector<std::optional<RobotPath>> paths;
 };
 
-struct ApproachPlanningResultStats {
+using ApproachPlanningMethodFn = std::function<ApproachPlanningResults(const ApproachPlanningProblem &,
+																	   random_numbers::RandomNumberGenerator &)>;
 
-	struct PathStats {
-		bool is_collision_free;
-		double length;
+ApproachPlanningMethodFn probing_by_pullout() {
+	return [](const ApproachPlanningProblem &problem,
+			  random_numbers::RandomNumberGenerator &rng) -> ApproachPlanningResults {
+
+		std::vector<std::optional<RobotPath>> paths;
+
+		for (const auto &target: problem.tree_model.target_points) {
+			auto ap = approach_planning::plan_approach_by_pullout(
+					*problem.tree_model.tree_collision_object,
+					*problem.tree_model.tree_convex_hull,
+					target,
+					0.1,
+					problem.robot,
+					rng,
+					1000
+			);
+
+			if (ap) {
+				paths.emplace_back(ap->path);
+			} else {
+				paths.emplace_back(std::nullopt);
+			}
+		}
+
+		return {
+				.paths = paths
+		};
 	};
+}
 
-};
+REGISTER_BENCHMARK(approach_planning_comparison) {
 
-using ApproachPlanningMethodFn = std::function<ApproachPlanningResults(const ApproachPlanningProblem &)>;
-
-REGISTER_BENCHMARK(approach_planning) {
+	robot_model::RobotModel robot_model = mgodpl::experiments::createProceduralRobotModel(
+			{
+					.total_arm_length = 1.0,
+					.joint_types = {experiments::JointType::HORIZONTAL},
+					.add_spherical_wrist = false
+			});
 
 	// Grab a list of all tree models:
 	auto tree_models = experiments::loadAllTreeBenchmarkData(results);
 
+	// Drop all targets already outside the tree:
+	for (auto &tree_model: tree_models) {
+		CGAL::Side_of_triangle_mesh<cgal::Surface_mesh, cgal::K> inside_outside_check(
+				tree_model.tree_convex_hull->convex_hull);
+
+		erase_if(tree_model.target_points, [&](const math::Vec3d &target) {
+			return inside_outside_check(cgal::to_cgal_point(target)) == CGAL::ON_UNBOUNDED_SIDE;
+		});
+	}
+
 	const size_t REPETITIONS = 10;
 
 	const std::vector<ApproachPlanningMethodFn> methods{
-			[](const ApproachPlanningProblem &problem) -> ApproachPlanningResults {
-				return {
-						.paths = {}
-				};
-			}
+			probing_by_pullout()
 	};
 
 	std::vector<ApproachPlanningProblem> problems;
 	problems.reserve(tree_models.size());
 	for (const auto &tree_model: tree_models) {
-		problems.emplace_back(ApproachPlanningProblem{tree_model});
+		problems.emplace_back(ApproachPlanningProblem{robot_model, tree_model});
 	}
 
 	struct Run {
@@ -76,18 +117,30 @@ REGISTER_BENCHMARK(approach_planning) {
 	std::for_each(std::execution::par, runs.begin(), runs.end(), [&](const Run &run) {
 		in_flight += 1;
 
+		// Grab the RNG, seed it with the repetition index:
+		random_numbers::RandomNumberGenerator rng(run.repetition_index);
+
 		std::cout << "Starting run " << run.method_index << " " << run.problem_index << " " << run.repetition_index
 				  << std::endl;
 		std::cout << "In flight: " << in_flight << std::endl;
 
 		const auto &method = methods[run.method_index];
 		const auto &problem = problems[run.problem_index];
-		const auto &result = method(problem);
+
+		// Record start time:
+		auto start_time = std::chrono::high_resolution_clock::now();
+		const auto &result = method(problem, rng);
+		// Record end time:
+		auto end_time = std::chrono::high_resolution_clock::now();
+
+		// Time elapsed in milliseconds:
+		auto time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
 
 		Json::Value result_json;
 		result_json["method"] = run.method_index;
 		result_json["problem"] = run.problem_index;
 		result_json["repetition"] = run.repetition_index;
+		result_json["time_ms"] = time_ms;
 
 		{
 
