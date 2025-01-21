3b9c69aea40bb2bd7733b837d19a5c5b9f882c2e
Werner Kroneman <werner@wernerkroneman.nl>
2025-01-21 13:09:04 +0100

    Got a basic animation going.

--- src/experiments/swaying_tree_branches.cpp
+++ src/experiments/swaying_tree_branches.cpp
@@ -2,6 +2,8 @@
 //
 // All rights reserved.
 
+#include <ranges>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Polyhedron_3.h>
 #include <CGAL/Surface_mesh/Surface_mesh.h>
@@ -95,6 +97,200 @@ void separate_and_union_components(CMesh &polyhedron) {
 	polyhedron = union_mesh;
 }
 
+Skeleton_vertex find_lowest_point(const Skeleton &skeleton) {
+	double min_distance = std::numeric_limits<double>::max();
+	Skeleton_vertex closest_vertex;
+	auto [start, end] = vertices(skeleton);
+	for (auto itr = start; itr != end; ++itr) {
+		double distance = skeleton[*itr].point.x() * skeleton[*itr].point.x() + skeleton[*itr].point.y() * skeleton[*
+			                  itr].point.y() + skeleton[*itr].point.z() * skeleton[*itr].point.z();
+		if (distance < min_distance) {
+			closest_vertex = itr - start;
+			min_distance = distance;
+		}
+	}
+	return closest_vertex;
+}
+
+// struct edge_weight_map {
+// 	using edge_descriptor = Skeleton::edge_descriptor;
+// 	using value_type = double;
+// 	using reference = double;
+// 	using key_type = edge_descriptor;
+// 	using category = boost::readable_property_map_tag;
+//
+// 	edge_weight_map(const Skeleton &skeleton) : skeleton(skeleton) {
+// 	}
+//
+// 	double operator[](const edge_descriptor &e) const {
+// 		auto v1 = skeleton[source(e, skeleton)].point;
+// 		auto v2 = skeleton[target(e, skeleton)].point;
+// 		return std::sqrt(std::pow(v1.x() - v2.x(), 2) + std::pow(v1.y() - v2.y(), 2) + std::pow(v1.z() - v2.z(), 2));
+// 	}
+//
+// 	const Skeleton &skeleton;
+// };
+
+std::pair<std::vector<double>, std::vector<Skeleton_vertex> > dijkstra_skeleton(
+	const Skeleton &skeleton,
+	Skeleton_vertex closest_vertex) {
+	std::vector<double> distances(num_vertices(skeleton));
+	std::vector<Skeleton_vertex> predecessors(num_vertices(skeleton));
+
+	auto weight_map = boost::make_function_property_map<Skeleton::edge_descriptor>(
+		[&skeleton](const Skeleton::edge_descriptor &e) -> double {
+			auto v1 = skeleton[source(e, skeleton)].point;
+			auto v2 = skeleton[target(e, skeleton)].point;
+			return sqrt((v1 - v2).squared_length());
+		});
+
+	boost::dijkstra_shortest_paths(skeleton,
+	                               closest_vertex,
+	                               boost::weight_map(weight_map)
+	                               .distance_map(
+		                               boost::make_iterator_property_map(
+			                               distances.begin(),
+			                               get(boost::vertex_index, skeleton)))
+	                               .predecessor_map(
+		                               boost::make_iterator_property_map(
+			                               predecessors.begin(),
+			                               get(boost::vertex_index, skeleton))));
+	return {distances, predecessors};
+}
+
+std::vector<std::pair<math::Vec3d, math::Vec3d> > extract_lines_from_skeleton(const Skeleton &skeleton) {
+	std::vector<std::pair<math::Vec3d, math::Vec3d> > lines;
+	for (const auto &edge: skeleton.m_edges) {
+		auto v1 = skeleton[source(edge, skeleton)].point;
+		auto v2 = skeleton[target(edge, skeleton)].point;
+
+		auto pair = std::make_pair(math::Vec3d(v1.x(), v1.y(), v1.z()), math::Vec3d(v2.x(), v2.y(), v2.z()));
+		lines.push_back(pair);
+	}
+	return lines;
+}
+
+math::Vec3d project_onto_skeleton_edge(const Skeleton &skeleton,
+                                       Skeleton::edge_descriptor edge,
+                                       const math::Vec3d &vertex) {
+	auto v1 = skeleton[source(edge, skeleton)].point;
+	auto v2 = skeleton[target(edge, skeleton)].point;
+
+	math::Vec3d p1(v1.x(), v1.y(), v1.z());
+	math::Vec3d p2(v2.x(), v2.y(), v2.z());
+	math::Vec3d p3(vertex.x(), vertex.y(), vertex.z());
+
+	double a = (p2 - p1).norm();
+	double b = (p3 - p1).norm();
+
+	math::Vec3d projection = p1 + (p2 - p1) * (b / a);
+	return projection;
+}
+
+Skeleton::edge_descriptor find_closest_edge_on_skeleton(const Skeleton &skeleton, const math::Vec3d &vertex) {
+	Skeleton::edge_descriptor closest_edge;
+	double min_distance = std::numeric_limits<double>::max();
+
+	const auto [start, end] = edges(skeleton);
+	for (auto itr = start; itr != end; ++itr) {
+		math::Vec3d p3(vertex.x(), vertex.y(), vertex.z());
+		math::Vec3d projection = project_onto_skeleton_edge(skeleton, *itr, vertex);
+
+		double distance = (p3 - projection).norm();
+
+		if (distance < min_distance) {
+			min_distance = distance;
+			closest_edge = *itr;
+		}
+	}
+
+	return closest_edge;
+}
+
+double find_dijkstra_distance(const Skeleton &skeleton,
+                              const math::Vec3d &vertex,
+                              const std::vector<double> &distances) {
+	Skeleton::edge_descriptor closest_edge = find_closest_edge_on_skeleton(skeleton, vertex);
+
+	double source_distance = distances[source(closest_edge, skeleton)];
+	double target_distance = distances[target(closest_edge, skeleton)];
+
+	// Find the closest point on the edge:
+	math::Vec3d projection = project_onto_skeleton_edge(skeleton, closest_edge, vertex);
+
+	double distance_from_source = (projection - math::Vec3d(
+		                               skeleton[source(closest_edge, skeleton)].point.x(),
+		                               skeleton[source(closest_edge, skeleton)].point.y(),
+		                               skeleton[source(closest_edge, skeleton)].point.z())).norm();
+
+	double distance_from_target = (projection - math::Vec3d(
+		                               skeleton[target(closest_edge, skeleton)].point.x(),
+		                               skeleton[target(closest_edge, skeleton)].point.y(),
+		                               skeleton[target(closest_edge, skeleton)].point.z())).norm();
+
+	double dijkstra_distance_1 = distance_from_source + source_distance;
+	double dijkstra_distance_2 = distance_from_target + target_distance;
+
+	return std::min(dijkstra_distance_1, dijkstra_distance_2);
+}
+
+std::vector<Skeleton::vertex_descriptor> create_hierarchical_order(const Skeleton &skeleton,
+                                                                   const std::vector<double> &distances) {
+	std::vector<Skeleton::vertex_descriptor> hierarchical_order;
+	hierarchical_order.reserve(num_vertices(skeleton));
+	// Fill with indices 0 to n-1:
+	for (std::size_t i = 0; i < num_vertices(skeleton); ++i) {
+		hierarchical_order.push_back(i);
+	}
+	// Sort by the dijkstra distance:
+	std::ranges::sort(hierarchical_order,
+	                  [&distances](Skeleton::vertex_descriptor a, Skeleton::vertex_descriptor b) {
+		                  return distances[a] < distances[b];
+	                  });
+	return hierarchical_order;
+}
+
+std::vector<math::Vec3d> compute_deltas(const Skeleton &skeleton,
+                                        const std::vector<Skeleton_vertex> &predecessors) {
+	std::vector<math::Vec3d> predecessor_deltas;
+	predecessor_deltas.reserve(num_vertices(skeleton));
+	for (std::size_t i = 0; i < num_vertices(skeleton); ++i) {
+		Skeleton_vertex predecessor = predecessors[i];
+		if (predecessor == i) {
+			predecessor_deltas.push_back({0, 0, 0});
+		} else {
+			math::Vec3d predecessor_point(skeleton[predecessor].point.x(),
+			                              skeleton[predecessor].point.y(),
+			                              skeleton[predecessor].point.z());
+			math::Vec3d current_point(skeleton[i].point.x(),
+			                          skeleton[i].point.y(),
+			                          skeleton[i].point.z());
+			predecessor_deltas.push_back(current_point - predecessor_point);
+		}
+	}
+	return predecessor_deltas;
+}
+
+math::Vec3d toVec3d(const K::Point_3 &point) {
+	return {point.x(), point.y(), point.z()};
+}
+
+mgodpl::Mesh ground_plane_2(double size) {
+	mgodpl::Mesh ground;
+	ground.vertices = {
+		{-size, -size, 0},
+		{size, -size, 0},
+		{size, size, 0},
+		{-size, size, 0}
+	};
+	ground.triangles = {
+		{0, 1, 2},
+		{0, 2, 3}
+	};
+	return ground;
+}
+
+
 REGISTER_VISUALIZATION(tree_skeletonization) {
 	// Load the apple tree trunk model:
 	auto trunk = tree_meshes::loadTreeMeshes("appletree1").trunk_mesh;
@@ -123,26 +319,106 @@ REGISTER_VISUALIZATION(tree_skeletonization) {
 	// get the correspondent surface points
 	mcs.convert_to_skeleton(skeleton);
 
+	// Find the point closest to 0:
+	Skeleton_vertex closest_vertex = find_lowest_point(skeleton);
+
+	// Perform Dijkstra's algorithm on the skeleton to find the shortest paths from the closest vertex
+	const auto &[distances, predecessors] = dijkstra_skeleton(skeleton, closest_vertex);
+
+	// Find the maximum Dijkstra distance
+	double max_dijkstra_distance = *std::ranges::max_element(distances);
+
+	// Create a hierarchical order of the skeleton vertices based on the Dijkstra distances
+	auto hierarchical_order = create_hierarchical_order(skeleton, distances);
+
+	// Compute the deltas between each vertex and its predecessor in the shortest path tree
+	auto rest_deltas = compute_deltas(skeleton, predecessors);
+
+
 	VtkLineSegmentsVisualization skeleton_viz(1, 0, 0);
 	viewer.addActor(skeleton_viz.getActor());
 
-	std::vector<std::pair<math::Vec3d, math::Vec3d> > lines;
-	for (const auto &edge: skeleton.m_edges) {
-		auto v1 = skeleton[source(edge, skeleton)].point;
-		auto v2 = skeleton[target(edge, skeleton)].point;
 
-		auto pair = std::make_pair(math::Vec3d(v1.x(), v1.y(), v1.z()), math::Vec3d(v2.x(), v2.y(), v2.z()));
-		lines.push_back(pair);
-	}
-	skeleton_viz.updateLine(lines);
+	// // Convert it back from the polyhedron:
+	// SMesh cgal_mesh_back;
+	// CGAL::copy_face_graph(polyhedron, cgal_mesh_back);
+	//
+	// Mesh trunk_mesh = convert_from_cgal_surface_mesh(cgal_mesh_back);
+	//
+	// // For each vertex in the mesh, find the Dijkstra distance to the lowest point:
+	// std::vector<double> dijkstra_distances;
+	// dijkstra_distances.reserve(trunk_mesh.vertices.size());
+	// for (const auto &vertex: trunk_mesh.vertices) {
+	// 	double dijkstra_distance = find_dijkstra_distance(skeleton, vertex, distances);
+	// 	dijkstra_distances.push_back(dijkstra_distance);
+	// }
+	// double max_dijkstra_distance = *std::ranges::max_element(dijkstra_distances);
 
-	// Convert it back from the polyhedron:
-	SMesh cgal_mesh_back;
-	CGAL::copy_face_graph(polyhedron, cgal_mesh_back);
+	// // At every vertex, add a sphere with radius proportional to dijkstra_distance / max_dijkstra_distance:
+	// for (std::size_t i = 0; i < trunk_mesh.vertices.size(); ++i) {
+	// 	viewer.addSphere(0.1 * dijkstra_distances[i] / max_dijkstra_distance, trunk_mesh.vertices[i], {0, 1, 0}, 4);
+	// }
 
-	Mesh trunk_mesh = convert_from_cgal_surface_mesh(cgal_mesh_back);
+	// Mesh trunk_mesh_deformed = trunk_mesh;
+	// for (std::size_t i = 0; i < trunk_mesh.vertices.size(); ++i) {
+	// 	trunk_mesh_deformed.vertices[i] = trunk_mesh.vertices[i] + math::Vec3d(
+	// 		                                  0,
+	// 		                                  0,
+	// 		                                  dijkstra_distances[i] / max_dijkstra_distance);
+	// }
 
-	viewer.addMesh(trunk_mesh, {0.5, 0.5, 0.5}, 0.5);
+	// viewer.addMesh(trunk_mesh, {0.5, 0.5, 0.5}, 0.5);
+	// viewer.addMesh(trunk_mesh_deformed, {1.0, 0.0, 1.0}, 0.5);
+
+	viewer.lockCameraUp();
+
+	viewer.addMesh(ground_plane_2(10), {0.8, 0.8, 0.8}, 1.0);
+
+	viewer.setCameraTransform({1.0, 5.0, 2.5}, {0.0, 0.0, 1.5});
+
+	double t = 0.0;
+	viewer.addTimerCallback([&] {
+		t += 0.1;
+		math::Vec3d wind_direction(sin(t) * sin(t * 0.74) * (0.5 * 0.5 * sin(t * 0.1)), 0, 0);
+
+		std::vector<math::Vec3d> new_deltas;
+		new_deltas.reserve(num_vertices(skeleton));
+		// Ajust the deltas:
+		for (std::size_t i = 0; i < num_vertices(skeleton); ++i) {
+			double delta_length = rest_deltas[i].norm();
+			math::Vec3d delta = rest_deltas[i];
+			math::Vec3d new_delta = delta + wind_direction * delta_length * (distances[i] / max_dijkstra_distance);
+			// adjust the length:
+			new_delta = new_delta / new_delta.norm() * delta_length;
+			new_deltas.push_back(new_delta);
+		}
+
+		std::vector<math::Vec3d> new_points(num_vertices(skeleton));
+		new_points[hierarchical_order[0]] = toVec3d(skeleton.m_vertices[hierarchical_order[0]].m_property.point);
+		for (std::size_t i: std::ranges::subrange(hierarchical_order.begin() + 1, hierarchical_order.end())) {
+			new_points[i] = new_points[predecessors[i]] + new_deltas[i];
+		}
+
+		// // At every skeleton vertex, put a sphere of radius proportional to the distance to the lowest point:
+		// auto [start, end] = vertices(skeleton);
+		// for (auto vertex = start; vertex != end; ++vertex) {
+		// 	math::Vec3d point(skeleton[*vertex].point.x(), skeleton[*vertex].point.y(), skeleton[*vertex].point.z());
+		// 	viewer.addSphere(0.1 * distances[*vertex] / max_dijkstra_distance, point, {0, 1, 0}, 4);
+		// }
+
+		std::vector<std::pair<math::Vec3d, math::Vec3d> > lines;
+		for (const auto &edge: skeleton.m_edges) {
+			auto v1 = new_points[source(edge, skeleton)];
+			auto v2 = new_points[target(edge, skeleton)];
+			lines.push_back(std::make_pair(v1, v2));
+		}
+
+		skeleton_viz.updateLine(lines);
+
+		if (t > 10 && viewer.isRecording()) {
+			viewer.stop();
+		}
+	});
 
 	viewer.start();
 }
